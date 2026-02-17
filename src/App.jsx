5a5004316b7fcb9ad1a988aeab08c6bb87174189
midnightph <pedrorodacinski26@gmail.com>
2026-02-17 20:06:37 -0300

    feat(auth-ui): redesign login interface with glassmorphism, animations and enhanced interactions

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,28 @@ function App() {
       </div>
       <div className='main'>
         <div className='card'>
-          <h1>Seja bem-vindo</h1>
+          <h1 style={{ paddingBottom: '20px' }}>Seja bem-vindo</h1>
 
           <div className='inputs'>
-          <input type="text" placeholder='Email' />
-          <input type="password" placeholder='Senha' />
+            <input type="text" placeholder='Email' />
+            <input type="password" placeholder='Senha' />
           </div>
+
+          <button type='submit'>Entrar</button>
+
+          <button className="google-btn">
+            <img
+              src="https://developers.google.com/identity/images/g-logo.png"
+              alt="Google"
+            />
+            Entrar com Google
+          </button>
+          <div className='divider'></div>
+
+          <h2>Ainda não possui uma conta?</h2>
+          <a href="">Cadastre-se</a>
         </div>
+
       </div>
     </>
   )
