f3a72e9b631a3ed80fe3be0e83310b3612d56fb2
midnightph <pedrorodacinski26@gmail.com>
2026-02-19 08:02:45 -0300

    refactor(routing): implement protected routes with PrivateRoute
    - Set Home as root route ("/")
    - Created PrivateRoute to handle authentication guard
    - Centralized auth check using onAuthStateChanged
    - Redirect unauthenticated users to /login
    - Moved route protection logic out of Home component
    - Organized PrivateRoute inside routes directory

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,13 +2,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "../pages/login";
 import Home from "../pages/home/index";
 import SignUp from "../pages/signUp";
+import PrivateRoute from "../components/privateRoute";
 
 export default function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={
+          <PrivateRoute>
+            <Home />
+          </PrivateRoute>
+        } />
+        <Route path="/login" element={<Login />} />
         <Route path="/signUp" element={<SignUp />} />
       </Routes>
     </BrowserRouter>
